70feead0d5678c8c65a2435c215082d35b327efd
William Kamp <manimaul@gmail.com>
2016-10-06 07:07:36 -0700

    breaking mxmcc into functions

--- mxmcc.py
+++ mxmcc.py
@@ -33,13 +33,7 @@ PROFILE_MB_C = 'MB_CHARTS'  # renders each chart as mbtiles file
 PROFILE_MB_R = 'MB_REGION'  # renders entire region as mbtiles file
 
 
-def compile_region(region, profile=PROFILE_MX_R, perform_clean=True):
-    region = region.upper()
-    profile = profile.upper()
-
-    checkpoint_store = CheckPointStore()
-    # ------------------------------------------------------------------------------------------------------------------
-
+def _build_catalog(checkpoint_store, profile, region):
     # build catalog
     point = CheckPoint.CHECKPOINT_CATALOG
     if checkpoint_store.get_checkpoint(region, profile) < point:
@@ -57,8 +51,9 @@ def compile_region(region, profile=PROFILE_MX_R, perform_clean=True):
         checkpoint_store.clear_checkpoint(region, profile, point)
     else:
         print 'skipping checkpoint', point
-    # ------------------------------------------------------------------------------------------------------------------
 
+
+def _create_tiles(checkpoint_store, profile, region):
     # create tiles
     point = CheckPoint.CHECKPOINT_TILE_VERIFY
     if checkpoint_store.get_checkpoint(region, profile) < point:
@@ -72,116 +67,152 @@ def compile_region(region, profile=PROFILE_MX_R, perform_clean=True):
         checkpoint_store.clear_checkpoint(region, profile, point)
     else:
         print 'skipping checkpoint', point
+
+
+def _merge_tiles(checkpoint_store, profile, region):
+    # merge
+    point = CheckPoint.CHECKPOINT_MERGE
+    if checkpoint_store.get_checkpoint(region, profile) < point:
+        print 'merging tiles for:', region
+        tilesmerge.merge_catalog(region)
+        checkpoint_store.clear_checkpoint(region, profile, point)
+    else:
+        print 'skipping checkpoint', point
+
+
+def _optimize_tiles(checkpoint_store, profile, region):
+    # optimize
+    point = CheckPoint.CHECKPOINT_OPT
+    if checkpoint_store.get_checkpoint(region, profile) < point:
+        if platform.system() == 'Windows':
+            tiles_opt.set_nothreads()
+        tiles_opt.optimize_dir(os.path.join(config.merged_tile_dir, region))
+
+        # verify all optimized tiles are there
+        if not verify.verify_opt(region):
+            raise Exception(region + ' was not optimized fully')
+
+        checkpoint_store.clear_checkpoint(region, profile, point)
+    else:
+        print 'skipping checkpoint', point
+
+
+def _should_encrypt(region):
+    encrypted_providers = {regions.provider_wavey_lines, regions.provider_ukho}
+    return regions.provider_for_region(region) in encrypted_providers
+
+
+def _encrypt_region(checkpoint_store, profile, region):
+    print 'encrypting tiles for region:', region
+    # encryption
+    point = CheckPoint.CHECKPOINT_ENCRYPTED
+    if checkpoint_store.get_checkpoint(region, profile) < point:
+        if not encryption_shim.encrypt_region(region):
+            raise Exception('encryption failed!')
+
+        checkpoint_store.clear_checkpoint(region, profile, point)
+    else:
+        print 'skipping checkpoint', point
+
+
+def _create_gemf(checkpoint_store, profile, region):
+    point = CheckPoint.CHECKPOINT_ARCHIVE
+    if checkpoint_store.get_checkpoint(region, profile) < point:
+        print 'archiving gemf for region:', region
+        should_encrypt = _should_encrypt(region)
+        if should_encrypt:
+            name = region + '.enc'
+        else:
+            name = region + '.opt'
+        gemf.generate_gemf(name, add_uid=should_encrypt)
+        if should_encrypt:
+            encryption_shim.generate_token(region)
+        checkpoint_store.clear_checkpoint(region, profile, point)
+    else:
+        print 'skipping checkpoint', point
+
+
+def _create_zdat(checkpoint_store, profile, region):
+    point = CheckPoint.CHECKPOINT_METADATA
+    if checkpoint_store.get_checkpoint(region, profile) < point:
+        print 'building zdat metadata archive for:', region
+        zdata.generate_zdat_for_catalog(region)
+        checkpoint_store.clear_checkpoint(region, profile, point)
+    else:
+        print 'skipping checkpoint', point
+
+
+def _fill_tiles(region):
+    # fill
+    # print 'filling tile \"holes\"', region
+    # filler.fill_all_in_region(region)
+    print region, 'fill skipped'
+
+
+def _create_region_mb_tiles(checkpoint_store, profile, region):
+    point = CheckPoint.CHECKPOINT_ARCHIVE
+    if checkpoint_store.get_checkpoint(region, profile) < point:
+        print 'archiving mbtiles for region:', region
+        region_dir = os.path.join(config.merged_tile_dir, region + '.opt')
+        mbtiles_file = os.path.join(config.compiled_dir, region + '.mbtiles')
+        if os.path.isfile(mbtiles_file):
+            os.remove(mbtiles_file)
+        mb.disk_to_mbtiles(region_dir, mbtiles_file, format='png', scheme='xyz')
+
+        checkpoint_store.clear_checkpoint(region, profile, point)
+    else:
+        print 'skipping checkpoint', point
+
+
+def __create_chart_mb_tiles(region):
+    region_charts_dir = os.path.join(config.unmerged_tile_dir, region)
+    for chart in os.listdir(region_charts_dir):
+        print 'archiving mbtiles for chart:', chart
+        chart_dir = os.path.join(region_charts_dir, chart)
+        prefix = re.sub(r'\W+', '_', chart).lower()
+        mbtiles_file = os.path.join(config.compiled_dir, prefix + '.mbtiles')
+        if os.path.isfile(mbtiles_file):
+            os.remove(mbtiles_file)
+        mb.disk_to_mbtiles(chart_dir, mbtiles_file, format='png', scheme='xyz')
+
+
+def _create_chart_mb_tiles(checkpoint_store, profile, region):
+    point = CheckPoint.CHECKPOINT_ARCHIVE
+    if checkpoint_store.get_checkpoint(region, profile) < point:
+        __create_chart_mb_tiles(region)
+        checkpoint_store.clear_checkpoint(region, profile, point)
+    else:
+        print 'skipping checkpoint', point
+
+
+def compile_region(region, profile=PROFILE_MX_R, perform_clean=True):
+    region = region.upper()
+    profile = profile.upper()
+
+    checkpoint_store = CheckPointStore()
+
+    _build_catalog(checkpoint_store, profile, region)
+
+    _create_tiles(checkpoint_store, profile, region)
     # ------------------------------------------------------------------------------------------------------------------
 
     if 'REGION' in profile:
-        # merge
-        point = CheckPoint.CHECKPOINT_MERGE
-        if checkpoint_store.get_checkpoint(region, profile) < point:
-            print 'merging tiles for:', region
-            tilesmerge.merge_catalog(region)
-            checkpoint_store.clear_checkpoint(region, profile, point)
-        else:
-            print 'skipping checkpoint', point
-        # --------------------------------------------------------------------------------------------------------------
-
-        # fill
-        # print 'filling tile \"holes\"', region
-        # filler.fill_all_in_region(region)
-
-        # optimize
-        point = CheckPoint.CHECKPOINT_OPT
-        if checkpoint_store.get_checkpoint(region, profile) < point:
-            if platform.system() == 'Windows':
-                tiles_opt.set_nothreads()
-            tiles_opt.optimize_dir(os.path.join(config.merged_tile_dir, region))
-
-            # verify all optimized tiles are there
-            if not verify.verify_opt(region):
-                raise Exception(region + ' was not optimized fully')
-
-            checkpoint_store.clear_checkpoint(region, profile, point)
-        else:
-            print 'skipping checkpoint', point
-        # --------------------------------------------------------------------------------------------------------------
+        _merge_tiles(checkpoint_store, profile, region)
+        _fill_tiles(region)
+        _optimize_tiles(checkpoint_store, profile, region)
 
         if 'MX_' in profile:
-            encrypted_providers = {regions.provider_wavey_lines, regions.provider_ukho}
-            should_encrypt = regions.provider_for_region(region) in encrypted_providers
+            should_encrypt = _should_encrypt(region)
             if should_encrypt:
-                print 'encrypting tiles for region:', region
-                # encryption
-                point = CheckPoint.CHECKPOINT_ENCRYPTED
-                if checkpoint_store.get_checkpoint(region, profile) < point:
-                    if not encryption_shim.encrypt_region(region):
-                        raise Exception('encryption failed!')
-
-                    checkpoint_store.clear_checkpoint(region, profile, point)
-                else:
-                    print 'skipping checkpoint', point
-                name = region + '.enc'
-            else:
-                name = region + '.opt'
-
-            # ----------------------------------------------------------------------------------------------------------
-
-            # gemf
-            point = CheckPoint.CHECKPOINT_ARCHIVE
-            if checkpoint_store.get_checkpoint(region, profile) < point:
-                print 'archiving gemf for region:', region
-                gemf.generate_gemf(name, add_uid=should_encrypt)
-
-                if should_encrypt:
-                    encryption_shim.generate_token(region)
-
-                checkpoint_store.clear_checkpoint(region, profile, point)
-            else:
-                print 'skipping checkpoint', point
-            # ----------------------------------------------------------------------------------------------------------
-
-            # zdat
-            point = CheckPoint.CHECKPOINT_METADATA
-            if checkpoint_store.get_checkpoint(region, profile) < point:
-                print 'building zdat metadata archive for:', region
-                zdata.generate_zdat_for_catalog(region)
-                checkpoint_store.clear_checkpoint(region, profile, point)
-            else:
-                print 'skipping checkpoint', point
-                # ----------------------------------------------------------------------------------------------------------
+                _encrypt_region(checkpoint_store, profile, region)
+            _create_gemf(checkpoint_store, profile, region)
+            _create_zdat(checkpoint_store, profile, region)
 
         if 'MB_' in profile:
-            # mbtiles
-            point = CheckPoint.CHECKPOINT_ARCHIVE
-            if checkpoint_store.get_checkpoint(region, profile) < point:
-                print 'archiving mbtiles for region:', region
-                region_dir = os.path.join(config.merged_tile_dir, region + '.opt')
-                mbtiles_file = os.path.join(config.compiled_dir, region + '.mbtiles')
-                if os.path.isfile(mbtiles_file):
-                    os.remove(mbtiles_file)
-                mb.disk_to_mbtiles(region_dir, mbtiles_file, format='png', scheme='xyz')
-
-                checkpoint_store.clear_checkpoint(region, profile, point)
-            else:
-                print 'skipping checkpoint', point
-                # ----------------------------------------------------------------------------------------------------------
+            _create_region_mb_tiles(checkpoint_store, profile, region)
 
     elif 'CHARTS' in profile and 'MB_' in profile:
-        # mbtiles
-        point = CheckPoint.CHECKPOINT_ARCHIVE
-        if checkpoint_store.get_checkpoint(region, profile) < point:
-            region_charts_dir = os.path.join(config.unmerged_tile_dir, region)
-            for chart in os.listdir(region_charts_dir):
-                print 'archiving mbtiles for chart:', chart
-                chart_dir = os.path.join(region_charts_dir, chart)
-                prefix = re.sub(r'\W+', '_', chart).lower()
-                mbtiles_file = os.path.join(config.compiled_dir, prefix + '.mbtiles')
-                if os.path.isfile(mbtiles_file):
-                    os.remove(mbtiles_file)
-                mb.disk_to_mbtiles(chart_dir, mbtiles_file, format='png', scheme='xyz')
-            checkpoint_store.clear_checkpoint(region, profile, point)
-        else:
-            print 'skipping checkpoint', point
-            # ----------------------------------------------------------------------------------------------------------
+        _create_chart_mb_tiles(checkpoint_store, profile, region)
 
     print 'final checkpoint', checkpoint_store.get_checkpoint(region, profile)
     if perform_clean and checkpoint_store.get_checkpoint(region, profile) > CheckPoint.CHECKPOINT_ENCRYPTED:
@@ -210,18 +241,31 @@ def print_usage():
 
 
 if __name__ == "__main__":
-    if config.check_dirs():
-        args = sys.argv
-        if len(args) < 2:
-            print_usage()
-        else:
-            rgn = args[1]
-            if len(args) >= 3:
-                prof = args[2]
-            else:
-                prof = PROFILE_MX_R
-            compile_region(rgn, prof)
-    else:
-        print 'Your mxmcc directory structure is not ready\n' + \
-              'Please edit the top portion of config.py, run config.py,\n' + \
-              'and place charts in their corresponding directories.'
+    r = 'REGION_FAA'
+    # compile_region(r, profile=PROFILE_MB_C, perform_clean=False)
+    __create_chart_mb_tiles(r)
+
+    # region_charts_dir = os.path.join(config.unmerged_tile_dir, 'REGION_FAA')
+    # for chart in os.listdir(region_charts_dir):
+    #     print 'archiving mbtiles for chart:', chart
+    #     chart_dir = os.path.join(region_charts_dir, chart)
+    #     mbtiles_file = os.path.join(config.compiled_dir, chart + '.mbtiles')
+    #     if os.path.isfile(mbtiles_file):
+    #         os.remove(mbtiles_file)
+    #     mb.disk_to_mbtiles(chart_dir, mbtiles_file, format='png', scheme='xyz')
+
+    # if config.check_dirs():
+    #     args = sys.argv
+    #     if len(args) < 2:
+    #         print_usage()
+    #     else:
+    #         rgn = args[1]
+    #         if len(args) >= 3:
+    #             prof = args[2]
+    #         else:
+    #             prof = PROFILE_MX_R
+    #         compile_region(rgn, prof)
+    # else:
+    #     print 'Your mxmcc directory structure is not ready\n' + \
+    #           'Please edit the top portion of config.py, run config.py,\n' + \
+    #           'and place charts in their corresponding directories.'
